afce0bf6b2f9dd9487b0687d83715b6b10e05892
takamitsu-iida <takamitsu.iida@gmail.com>
2024-09-17 18:45:21 +0900

    temp save

--- static/site/js/gpgpu-particles2.js
+++ static/site/js/gpgpu-particles2.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/controls/OrbitControls.js";
 
 // stats.js
 import Stats from "three/libs/stats.module.js";
@@ -8,10 +7,14 @@ import Stats from "three/libs/stats.module.js";
 // three.js/examples/jsm/misc/GPUComputationRenderer.js
 // three.js/examples/jsm/postprocessing/Pass.js
 
-import { GPUComputationRenderer, Variable } from "three/libs/misc/GPUComputationRenderer.js";
+import { GPUComputationRenderer } from "three/libs/misc/GPUComputationRenderer.js";
 
 // 参照元
 // https://github.com/Kanahiro/three-gpu-particle
+
+
+
+// TODO: wind.pngを消す
 
 
 const VIEW_WIDTH = 800;
@@ -41,6 +44,9 @@ export class Main {
 
   params = {
   }
+
+
+  gpuParticle;
 
   constructor(params) {
     this.params = Object.assign(this.params, params);
@@ -75,22 +81,22 @@ export class Main {
 
     // カメラ
     this.camera = new THREE.OrthographicCamera(
-      -1 * VIEW_WIDTH * 0.5,
+      VIEW_WIDTH * 0.5 * (-1),
       VIEW_WIDTH * 0.5,
       VIEW_HEIGHT * 0.5,
-      -1 * VIEW_HEIGHT * 0.5,
-      -1 * 1000,
+      VIEW_HEIGHT * 0.5 * (-1),
+      1000 * (-1),
       10000
     );
     this.camera.position.set(0, 0, 1);
 
     // レンダラ
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(this.sizes.width, this.sizes.height);
+    // 通常はコンテナのサイズに合わせるが、ここでは固定にする
+    // this.renderer.setSize(this.sizes.width, this.sizes.height);
+    this.renderer.setSize(VIEW_WIDTH, VIEW_HEIGHT);
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    this.renderer.setClearColor(0x000000);
     this.container.appendChild(this.renderer.domElement);
-
   }
 
 
@@ -125,9 +131,7 @@ export class Main {
       // gpgpuでの描画処理
       //
       {
-
-
-
+        this.gpuParticle.render();
       }
 
       // 再描画
@@ -142,97 +146,438 @@ export class Main {
     this.sizes.width = this.container.clientWidth;
     this.sizes.height = this.container.clientHeight;
 
-    this.camera.aspect = this.sizes.width / this.sizes.height;
-    this.camera.updateProjectionMatrix();
+    // this.camera.aspect = this.sizes.width / this.sizes.height;
+    // this.camera.updateProjectionMatrix();
 
-    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    this.renderer.setSize(this.sizes.width, this.sizes.height);
-
-    // シェーダー側に渡すUniform変数を変更する
-    this.particleUniforms.cameraConstant.value = this.getCameraConstant();
+    // this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    // this.renderer.setSize(this.sizes.width, this.sizes.height);
   }
 
 
-  // 現在の位置情報を決定するためのシェーダー
-  computeShaderPosition = /*glsl*/`
-    #define delta ( 1.0 / 60.0 )
+  // パーティクルの初期化
+  initParticles() {
 
-    void main() {
-      vec2 uv = gl_FragCoord.xy / resolution.xy;
-      vec4 tmpPos = texture2D( texturePosition, uv );
-      vec3 pos = tmpPos.xyz;
-      vec4 tmpVel = texture2D( textureVelocity, uv );
+    const velocityTexture = new THREE.TextureLoader().load('./static/site/img/wind.png');
+    velocityTexture.magFilter = THREE.NearestFilter;
 
-      // velが移動する方向(もう一つ下のcomputeShaderVelocityを参照)
-      vec3 vel = tmpVel.xyz;
+    this.gpuParticle = new GpuParticle(this.renderer, velocityTexture)
 
-      // 移動する方向に速度を掛け合わせた数値を現在地に加える。
-      pos += vel * delta;
-      gl_FragColor = vec4( pos, 1.0 );
-    }
-  `;
+    const material = new THREE.ShaderMaterial({
+      side: THREE.DoubleSide,
+      transparent: true,
+      uniforms: {
+        backgroundTexture: { value: velocityTexture },
+        particleTexture: { value: this.gpuParticle.getParticleTexture() },
+      },
+      vertexShader: /*glsl*/`
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          gl_Position =  projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+      `,
+      fragmentShader: /*glsl*/`
+        uniform sampler2D backgroundTexture;
+        uniform sampler2D particleTexture;
+        varying vec2 vUv;
+        void main() {
+          vec4 backgroundColor = texture2D(backgroundTexture, vUv);
+          vec4 particleColor = texture2D(particleTexture, vUv);
+          gl_FragColor = vec4(mix(backgroundColor.rgb, particleColor.rgb, particleColor.a), 1.0);
+        }
+      `,
+    });
 
+    const mesh = new THREE.Mesh(
+      new THREE.PlaneGeometry(VIEW_WIDTH, VIEW_HEIGHT),
+      material,
+    );
 
-  // 移動方向を計算するためのシェーダー
-  computeShaderVelocity = /*glsl*/`
-    #include <common>
+    this.scene.add(mesh);
+  }
 
-    // 今回はなにもしてない。
-    // ここでVelのx y zについて情報を上書きすると、それに応じて移動方向が変わる
-
-    void main() {
-      vec2 uv = gl_FragCoord.xy / resolution.xy;
-      float idParticle = uv.y * resolution.x + uv.x;
-      vec4 tmpVel = texture2D( textureVelocity, uv );
-      vec3 vel = tmpVel.xyz;
-
-      gl_FragColor = vec4( vel.xyz, 1.0 );
-    }
-  `;
-
-
-  // シェーダーマテリアルで描画するための頂点シェーダー
-  particleVertexShader = /*glsl*/`
-    #include <common>
-    uniform sampler2D texturePosition;
-    uniform float cameraConstant;
-    uniform float density;
-    varying vec4 vColor;
-    varying vec2 vUv;
-    uniform float radius;
-
-    void main() {
-        vec4 posTemp = texture2D( texturePosition, uv );
-        vec3 pos = posTemp.xyz;
-        vColor = vec4( 1.0, 0.7, 1.0, 1.0 );
-
-        // ポイントのサイズを決定
-        vec4 mvPosition = modelViewMatrix * vec4( pos, 1.0 );
-        gl_PointSize = 0.5 * cameraConstant / ( - mvPosition.z );
-
-        // uv情報の引き渡し
-        vUv = uv;
-
-        // 変換して格納
-        gl_Position = projectionMatrix * mvPosition;
-    }
-  `;
+}
 
 
-// シェーダーマテリアルで描画するためのフラグメントシェーダー
-  particleFragmentShader = /*glsl*/`
-    // VertexShaderから受け取った色を格納するだけ。
-    varying vec4 vColor;
-    void main() {
-      // 丸い形に色をぬるための計算
-      float f = length( gl_PointCoord - vec2( 0.5, 0.5 ) );
-      if ( f > 0.1 ) {
-          discard;
+class GpuParticle {
+
+  vertexTexture;
+  particleRenderer;
+
+  // velocityTextureはwind.png
+  constructor(renderer, velocityTexture, options = {}) {
+
+    const width = options.width || VIEW_WIDTH;
+    const height = options.height || VIEW_HEIGHT;
+
+    this.vertexTexture = new VertexTexture(
+      renderer,
+      velocityTexture,
+      {
+        width,
+        height,
+        particleSpeed: options.particleSpeed || 2,
+        particleCount: options.particleCount || 64,
+        dropFactor: options.dropFactor || 50,
+        repeat: options.repeat || false,
       }
-      gl_FragColor = vColor;
+    );
+
+    this.particleRenderer = new ParticleRenderer(
+      renderer,
+      this.vertexTexture.getTexture(),
+      {
+        width,
+        height,
+        particleSize: options.particleSize || 3,
+        trajectoryFactor: options.trajectoryFactor || 0.01,
+      },
+    );
+
+  }
+
+  setVelocityTexture(texture) {
+    this.vertexTexture.updateVelocityTexture(texture);
+  }
+
+  setParticleSpeed(speed) {
+    this.vertexTexture.updateParticleSpeed(speed);
+  }
+
+  getParticleTexture() {
+    return this.particleRenderer.getTexture();
+  }
+
+  render() {
+    this.vertexTexture.compute();
+    this.particleRenderer.render();
+  }
+
+}
+
+
+export class VertexTexture {
+
+  // GPUComputationRendererのインスタンス
+  gpuRenderer;
+
+  // gpuRenderer.addVariable()の戻り値
+  computationVariable;
+
+  // gpuRenderer.createTexture()の戻り値
+  vertexTexture;
+
+  // 引数で渡された wind.png
+  velocityTexture;
+
+
+  constructor(renderer, velocityTexture, options = {}) {
+
+    this.velocityTexture = velocityTexture;
+
+    const {
+      width,
+      height,
+      particleSpeed,
+      particleCount,
+      dropFactor,
+      repeat,
+    } = options;
+
+
+    this.gpuRenderer = new GPUComputationRenderer(
+      particleCount,
+      particleCount,
+      renderer,
+    );
+
+    this.vertexTexture = this.gpuRenderer.createTexture();
+
+    // 頂点の位置をランダムに初期化
+    // vec4の使い方は(x, y, age, velocity)と定義する
+    for (let i = 0; i < this.vertexTexture.image.data.length / 4; i++) {
+      this.vertexTexture.image.data[i * 4 + 0] = (Math.random() - 0.5) * width;  // x
+      this.vertexTexture.image.data[i * 4 + 1] = (Math.random() - 0.5) * height; // y
+      this.vertexTexture.image.data[i * 4 + 2] = Math.random() * dropFactor;     // age
+      this.vertexTexture.image.data[i * 4 + 3] = 0;                              // velocity
     }
-  `;
+
+    this.computationVariable = this.gpuRenderer.addVariable(
+      'computationTexture',
+      /*glsl*/`
+      precision highp float;
+
+      uniform sampler2D velocityTexture;
+      uniform float particleSpeed;
+      uniform float dropFactor;
+      uniform bool repeat;
+
+      float rand(vec2 p){
+          return fract(sin(dot(p, vec2(12.9898,78.233))) * 43758.5453);
+      }
+
+      vec3 getVelocity(vec2 pos) {
+          float xPx = 1.0 / ${width}.0;
+          float yPx = 1.0 / ${height}.0;
+          vec2 centerUv = vec2(pos.x / ${width}.0 + 0.5, pos.y / ${height}.0 + 0.5);
+          vec3 center = texture2D(velocityTexture, centerUv).rgb;
+          vec3 left = texture2D(velocityTexture, centerUv - vec2(xPx, 0.0)).rgb;
+          vec3 top = texture2D(velocityTexture, centerUv + vec2(0.0, yPx)).rgb;
+          vec3 right = texture2D(velocityTexture, centerUv + vec2(xPx, 0.0)).rgb;
+          vec3 bottom = texture2D(velocityTexture, centerUv - vec2(0.0, yPx)).rgb;
+
+          vec3 avg = (center + left + top + right + bottom) * 0.2 - vec3(vec2(0.49803922), 0.0);
+
+          return avg;
+      }
+
+      void main()	{
+
+          vec2 uv = gl_FragCoord.xy / resolution.xy;
+          vec4 position = texture2D(computationTexture, uv);
+
+          float age = position.z;
+
+          vec3 velocity = getVelocity(position.xy);
+          if (age > dropFactor) {
+            // reset particle position
+            vec2 random = vec2((rand(position.xy) - 0.5) * ${width}.0, (rand(position.yx) - 0.5) * ${height}.0);
+            gl_FragColor = vec4(random, 0.0, 0.0);
+          } else {
+            float absVelocity = length(velocity.xy);
+            vec2 newPosition = position.xy + velocity.xy * particleSpeed;
+            if (repeat) {
+              if (newPosition.x < -0.5 * ${width}.0) {
+                newPosition.x += 1.0 * ${width}.0;
+              } else if (0.5 * ${width}.0 < newPosition.x) {
+                newPosition.x -= 1.0 * ${width}.0;
+              }
+              if (newPosition.y < -0.5 * ${height}.0) {
+                newPosition.y += 1.0 * ${height}.0;
+              } else if (0.5 * ${height}.0 < newPosition.y) {
+                newPosition.y -= 1.0 * ${height}.0;
+              }
+            }
+            gl_FragColor = vec4(newPosition, age + rand(vec2(absVelocity, age)), absVelocity);
+          }
+      }
+      `,
+      this.vertexTexture,
+    );
+
+    this.computationVariable.material.uniforms = {
+      velocityTexture: { value: this.velocityTexture },
+      particleSpeed: { value: particleSpeed },
+      dropFactor: { value: dropFactor },
+      repeat: { value: repeat },
+    };
+
+    this.gpuRenderer.setVariableDependencies(this.computationVariable, [
+      this.computationVariable,
+    ]);
+
+    this.gpuRenderer.init();
+  }
+
+  getTexture() {
+    return this.gpuRenderer.getCurrentRenderTarget(this.computationVariable).texture;
+  }
+
+  updateVelocityTexture(texture) {
+    this.computationVariable.material.uniforms.velocityTexture.value = texture;
+  }
+
+  updateParticleSpeed(speed) {
+    this.computationVariable.material.uniforms.particleSpeed.value = speed;
+  }
+
+  updateDropFactor(dropFactor) {
+    this.computationVariable.material.uniforms.dropFactor.value = dropFactor;
+  }
+
+  compute() {
+    this.gpuRenderer.compute();
+  }
+}
 
 
 
+class ParticleRenderer {
+
+  renderer;
+
+  vertexTexture;
+
+  singleRenderTarget;
+
+  currRenderTarget;
+  prevRenderTarget;
+  renderTargetSwap;
+
+  camera;
+  singleScene;
+  singleMaterial;
+  mixScene;
+  mixMesh;
+
+  constructor(renderer, vertexTexture, options = {}) {
+    this.renderer = renderer;
+    this.vertexTexture = vertexTexture;
+
+    const { width, height, particleSize, trajectoryFactor } = options;
+
+    this.singleRenderTarget = new THREE.WebGLRenderTarget(
+      width,
+      height,
+      {
+        magFilter: THREE.LinearFilter,
+        minFilter: THREE.LinearFilter,
+        format: THREE.RGBAFormat,
+      });
+
+    this.currRenderTarget = new THREE.WebGLRenderTarget(
+      width,
+      height,
+      {
+        magFilter: THREE.LinearFilter,
+        minFilter: THREE.LinearFilter,
+        format: THREE.RGBAFormat,
+      });
+
+    this.prevRenderTarget = new THREE.WebGLRenderTarget(
+      width,
+      height,
+      {
+        magFilter: THREE.LinearFilter,
+        minFilter: THREE.LinearFilter,
+        format: THREE.RGBAFormat,
+      }
+    );
+
+    this.renderTargetSwap = this.prevRenderTarget;
+
+    this.singleScene = new THREE.Scene();
+
+    this.camera = new THREE.OrthographicCamera(
+      width / 2 * (-1),
+      width / 2,
+      height / 2,
+      height / 2 * (-1),
+      0,
+    );
+
+    const geometry = new THREE.BufferGeometry();
+    const vertices = new Float32Array(width * height * 3);
+    const uv = new Float32Array(width * height * 2);
+    let p = 0;
+    for (var j = 0; j < height; j++) {
+      for (var i = 0; i < width; i++) {
+        uv[p++] = i / (width - 1);
+        uv[p++] = j / (height - 1);
+      }
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setAttribute('uv', new THREE.BufferAttribute(uv, 2));
+
+    this.singleMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        posTexture: { value: null },
+        prevTexture: { value: null },
+        particleSize: { value: particleSize },
+      },
+      vertexShader: /*glsl*/`
+        uniform sampler2D posTexture;
+        uniform float particleSize;
+        varying vec2 vUv;
+        varying vec4 vPos;
+
+        void main() {
+          vUv = uv;
+          vPos = texture2D(posTexture, vUv); // xPost, yPost, age, velocity
+          gl_PointSize = particleSize;
+          gl_Position =  projectionMatrix * modelViewMatrix * vec4(vPos.xy, 0.0, 1.0);
+        }
+        `,
+      fragmentShader: /*glsl*/`
+        varying vec4 vPos;
+        void main() {
+          float f = length( gl_PointCoord - vec2( 0.5, 0.5 ) );
+          if ( f > 0.3 ) { discard; }
+
+          vec3 slowest = vec3(1.0, 1.0, 1.0);
+          vec3 fastest = vec3(1.0, 1.0, 1.0);
+          gl_FragColor = vec4(mix(slowest, fastest, vPos.w * 3.0), 1.0);
+        }
+      `,
+    });
+
+    const points = new THREE.Points(geometry, this.singleMaterial);
+    this.singleScene.add(points);
+
+    this.mixMesh = new THREE.Mesh(
+      new THREE.PlaneGeometry(width, height),
+      new THREE.ShaderMaterial({
+        uniforms: {
+          currTexture: { value: null },
+          prevTexture: { value: null },
+          trajectoryFactor: { value: trajectoryFactor },
+        },
+        vertexShader: /*glsl*/`
+          varying vec2 vUv;
+          void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+          }
+          `,
+        fragmentShader: /*glsl*/`
+          uniform sampler2D currTexture;
+          uniform sampler2D prevTexture;
+          uniform float trajectoryFactor;
+          varying vec2 vUv;
+
+          void main() {
+            vec4 curr = texture2D( currTexture, vUv );
+            vec4 prev = texture2D( prevTexture, vUv ) - vec4(vec3(0.0), trajectoryFactor);
+            gl_FragColor = curr + prev;
+          }
+        `,
+      }),
+    );
+
+    this.mixScene = new THREE.Scene();
+
+    this.mixScene.add(this.mixMesh);
+  }
+
+  updateVertexTexture(texture) {
+    this.vertexTexture = texture;
+  }
+
+  getTexture() {
+    return this.currRenderTarget.texture;
+  }
+
+  render() {
+    // update vertices
+    this.singleMaterial.uniforms.posTexture.value = this.vertexTexture;
+
+    // render current vertices
+    this.renderer.setRenderTarget(this.singleRenderTarget);
+    this.renderer.render(this.singleScene, this.camera);
+    this.renderer.setRenderTarget(null);
+
+    // overlay two texture as a gradient: current vertieces and vertices 1-frame ago
+    this.mixMesh.material.uniforms.currTexture.value = this.singleRenderTarget.texture;
+    this.mixMesh.material.uniforms.prevTexture.value = this.prevRenderTarget.texture;
+
+    this.renderer.setRenderTarget(this.currRenderTarget);
+    this.renderer.render(this.mixScene, this.camera);
+    this.renderer.setRenderTarget(null);
+
+    // swap
+    this.renderTargetSwap = this.currRenderTarget;
+    this.currRenderTarget = this.prevRenderTarget;
+    this.prevRenderTarget = this.renderTargetSwap;
+  }
 }
