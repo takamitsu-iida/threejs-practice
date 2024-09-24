c1bfab434f2978bf687518d6fcf468a42ef741a6
takamitsu-iida <takamitsu.iida@gmail.com>
2024-09-24 15:50:17 +0900

    minor fix

--- static/site/js/curve.js
+++ static/site/js/curve.js
@@ -1,6 +1,11 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/controls/OrbitControls.js";
+
+// lil-gui
 import { GUI } from "three/libs/lil-gui.module.min.js";
+
+// stats.js
+import Stats from "three/libs/stats.module.js";
 
 export class Main {
 
@@ -15,6 +20,8 @@ export class Main {
   camera;
   renderer;
   controller;
+
+  statsjs;
 
   renderParams = {
     clock: new THREE.Clock(),
@@ -31,35 +38,54 @@ export class Main {
   pointCoordinates = [
     [-6, 1, 10],
     [-1, 1, 10],
-    [3,  2,  4],
-    [6, 15,  4],
-    [6, 12,  4],
-    [15, 10,-15],
-    [15,  9,-16],
-    [17,  6,-16],
-    [10,  9,  7],
-    [2,  9,  8],
-    [-4,  8,  7],
-    [-8,  7,  1],
-    [-9,  7, -4],
-    [-6,  6, -9],
-    [0,  5,-10],
-    [7,  5, -7],
-    [7,  5,  0],
-    [0,  5,  2],
-    [-5,  4,  2],
-    [-7,  4, -5],
-    [-8,  3, -9],
-    [-12,  3, -10],
-    [-15,  2, -7],
-    [-15,  2, -2],
-    [-14,  1,  3],
-    [-11,  1, 10],
-    [-6,  1, 10]
+    [3, 2, 4],
+    [6, 15, 4],
+    [6, 12, 4],
+    [15, 10, -15],
+    [15, 9, -16],
+    [17, 6, -16],
+    [10, 9, 7],
+    [2, 9, 8],
+    [-4, 8, 7],
+    [-8, 7, 1],
+    [-9, 7, -4],
+    [-6, 6, -9],
+    [0, 5, -10],
+    [7, 5, -7],
+    [7, 5, 0],
+    [0, 5, 2],
+    [-5, 4, 2],
+    [-7, 4, -5],
+    [-8, 3, -9],
+    [-12, 3, -10],
+    [-15, 2, -7],
+    [-15, 2, -2],
+    [-14, 1, 3],
+    [-11, 1, 10],
+    [-6, 1, 10]
   ];
 
 
   constructor() {
+
+    // init scene, camera, renderer
+    this.initThreejs();
+
+    // stats.jsを初期化
+    this.initStatsjs();
+
+    // lil-gui
+    this.initGui();
+
+    // 曲線を描画する
+    this.drawCurve();
+
+    // フレーム毎の処理(requestAnimationFrameで再帰的に呼び出される)
+    this.render();
+  }
+
+
+  initThreejs() {
 
     // コンテナ
     this.container = document.getElementById("threejsContainer");
@@ -106,29 +132,25 @@ export class Main {
     //
     this.scene.add(new THREE.AxesHelper(100));
     this.scene.add(new THREE.GridHelper(50, 50));
+  }
 
-    //
-    // 点をもとにカーブを描く
-    //
 
-    const pointVectors = []
-    this.pointCoordinates.forEach(coords => {
-      pointVectors.push(new THREE.Vector3(coords[0], coords[1], coords[2]));
-    });
+  initStatsjs() {
+    let container = document.getElementById("statsjsContainer");
+    if (!container) {
+      container = document.createElement("div");
+      container.id = "statsjsContainer";
+      this.container.appendChild(container);
+    }
 
-    this.curve = new THREE.CatmullRomCurve3(pointVectors);
-    const points = this.curve.getPoints(this.params.pointNum);
+    this.statsjs = new Stats();
+    this.statsjs.dom.style.position = "relative";
+    this.statsjs.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    container.appendChild(this.statsjs.dom);
+  }
 
-    const geometry = new THREE.BufferGeometry();
-    geometry.setFromPoints(points);
 
-    const material = new THREE.LineBasicMaterial({
-      color: "yellow"
-    });
-
-    this.line = new THREE.LineLoop(geometry, material);
-    this.scene.add(this.line);
-
+  initGui() {
     // lil-gui
     const gui = new GUI({ width: 300 });
     gui
@@ -137,13 +159,10 @@ export class Main {
       .max(300)
       .step(1)
       .name("Number of points")
-      .onChange(()=>{
+      .onChange(() => {
         const points = this.curve.getPoints(this.params.pointNum);
         this.line.geometry.setFromPoints(points);
       });
-
-    // フレーム毎の処理(requestAnimationFrameで再帰的に呼び出される)
-    this.render();
   }
 
 
@@ -157,6 +176,9 @@ export class Main {
     }
 
     {
+      // stats.jsを更新
+      this.statsjs.update();
+
       // カメラコントローラーの更新
       this.controller.update();
 
@@ -178,5 +200,30 @@ export class Main {
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     this.renderer.setSize(this.sizes.width, this.sizes.height);
   };
+
+
+  drawCurve(){
+    //
+    // 点をもとにカーブを描く
+    //
+
+    const pointVectors = []
+    this.pointCoordinates.forEach(coords => {
+      pointVectors.push(new THREE.Vector3(coords[0], coords[1], coords[2]));
+    });
+
+    this.curve = new THREE.CatmullRomCurve3(pointVectors);
+    const points = this.curve.getPoints(this.params.pointNum);
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setFromPoints(points);
+
+    const material = new THREE.LineBasicMaterial({
+      color: "yellow"
+    });
+
+    this.line = new THREE.LineLoop(geometry, material);
+    this.scene.add(this.line);
+  }
 
 }
