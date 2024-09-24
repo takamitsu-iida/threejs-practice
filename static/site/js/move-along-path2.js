48263b043819e61bbef1c4cd3dbdf33abab37e31
takamitsu-iida <takamitsu.iida@gmail.com>
2024-09-24 10:26:14 +0900

    add move-along-path2

--- static/site/js/move-along-path2.js
+++ static/site/js/move-along-path2.js
@@ -1,6 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/controls/OrbitControls.js";
 
+// utils/BufferGeometryUtils.js
+import { mergeGeometries } from "three/libs/utils/BufferGeometryUtils.js";
+
 // stats.js
 import Stats from "three/libs/stats.module.js";
 
@@ -27,24 +30,28 @@ export class Main {
     interval: 1 / 30,  // = 30fps
   }
 
+  arrow;
+  verticalLine;
+
   params = {
     path: "./static/data/depth_map_data_20240727_0717_stripped.csv",
     csvData: null,
     meanLat: 0.0,
     meanLon: 0.0,
 
-    numPoints: 2000,
+    numPoints: 0,
 
-    // ボートの位置
+    // ボートの位置を表す0.0 ～ 1.0の値
     fraction: 0.0,
 
     // ボートの位置のvec3配列
-    boatCurvePoints: null,
+    boatPoints: null,
+
+    // ボートの位置の接線ベクトルのvec3配列
+    boatTangents: null,
 
     // 深度の位置のvec3配列
-    depthCurvePoints: null,
-
-
+    depthPoints: null,
   }
 
 
@@ -72,8 +79,14 @@ export class Main {
     // stats.jsを初期化
     this.initStatsjs();
 
-    // CSVデータをパスを初期化
+    // CSVデータを元にボートの軌跡となるパスを初期化
     this.initPath();
+
+    // ボートを表示
+    this.createArrow();
+
+    // 垂直線を表示
+    this.createVerticalLine();
 
     // フレーム毎の処理(requestAnimationFrameで再帰的に呼び出される)
     this.render();
@@ -95,7 +108,7 @@ export class Main {
       // CSVデータをパースして、this.params.csvDataに保存
       this.parseCsv(text);
 
-      console.log(this.params);
+      // console.log(this.params);
 
       // 瞬時にfetchできても0.5秒はローディング画面を表示する
       const interval = setInterval(() => {
@@ -225,6 +238,9 @@ export class Main {
       // カメラコントローラーの更新
       this.controller.update();
 
+      // ボートの位置を更新
+      this.updatePosition();
+
       // 再描画
       this.renderer.render(this.scene, this.camera);
     }
@@ -247,9 +263,10 @@ export class Main {
 
   initPath() {
     const csvData = this.params.csvData;
+
     // console.log(csvData);
 
-    const depthPoints = csvData.map((data) => {
+    const depthVector3List = csvData.map((data) => {
 
       // |       |             lat |             lon |        depth |
       // |:------|----------------:|----------------:|-------------:|
@@ -260,35 +277,66 @@ export class Main {
       // Z軸 緯度(lat) Three.jsのZ軸は下向きなので、マイナス方向にする
 
       const {lat, lon, depth} = data;
-      return new THREE.Vector3(lon, -depth, -lat);
-    });
-    // console.log(points);
 
-    const depthCurve = new THREE.CatmullRomCurve3(depthPoints);
-    const depthCurvePoints = depthCurve.getPoints(this.params.numPoints);
+      // 移動や拡大縮小はジオメトリを操作したほうが簡単だけど、
+      // ボートの位置とか向きを操作するので、先にデータを加工したほうがいい
+
+      // 拡大：　そのまま利用すると緯度経度の差が小さすぎるので拡大する
+      // 移動：　原点付近に寄せるために平均値を引く
+      const scaleFactor = 10000;
+      const latNormalized = (-lat + this.params.meanLat) * scaleFactor;
+      const lonNormalized = (lon - this.params.meanLon) * scaleFactor;
+      const depthNormalized = -depth;
+
+      return new THREE.Vector3(lonNormalized, depthNormalized, latNormalized);
+    });
+
+    this.params.numPoints = depthVector3List.length;
+
+    const depthCurve = new THREE.CatmullRomCurve3(depthVector3List);
+
+    // これで取得してもいいけど、接線ベクトルが取得できない
+    // const depthCurvePoints = depthCurve.getPoints(this.params.numPoints);
+
+    const depthPoints = [];
+    const boatPoints = [];
+    const boatTangents = [];
+    for (let i = 0; i < this.params.numPoints; i++) {
+      // 0.0 ～ 1.0 の間で、等間隔に点を取得
+      const fraction = i / this.params.numPoints;
+
+      // そのfractionでのVector3を取得
+      const point = depthCurve.getPointAt(fraction);
+      depthPoints.push(point);
+
+      // ボートの位置はY座標を0にしたもの
+      boatPoints.push(new THREE.Vector3(point.x, 0, point.z));
+
+      // そのfractionにおける接線ベクトルを取得
+      const tangent = depthCurve.getTangent(fraction);
+
+      // ボートは水面の上にいるので、Y方向を0にする
+      tangent.y = 0;
+
+      boatTangents.push(tangent);
+    }
 
     // 外からアクセスできるように保存しておく
-    this.params.depthCurvePoints = depthCurvePoints;
+    this.params.depthPoints = depthPoints;
+    this.params.boatPoints = boatPoints;
+    this.params.boatTangents = boatTangents;
 
     // 水深用のジオメトリ
-    const depthGeometry = new THREE.BufferGeometry().setFromPoints(depthCurvePoints);
-
-    const boatCurvePoints = depthCurvePoints.map((point) => {
-      return new THREE.Vector3(point.x, 0, point.z);
-    });
-
-    // 外からアクセスできるように保存しておく
-    this.params.boatCurvePoints = boatCurvePoints;
+    const depthGeometry = new THREE.BufferGeometry().setFromPoints(depthPoints);
 
     // ボート用のジオメトリ
-    const boatGeometry = new THREE.BufferGeometry().setFromPoints(boatCurvePoints);
+    const boatGeometry = new THREE.BufferGeometry().setFromPoints(boatPoints);
 
     // 原点に寄せて表示するためにジオメトリを移動、拡大する
-    depthGeometry.translate(-this.params.meanLon, 0, this.params.meanLat);
-    depthGeometry.scale(10000, 1, 10000);
-
-    boatGeometry.translate(-this.params.meanLon, 0, this.params.meanLat);
-    boatGeometry.scale(10000, 1, 10000);
+    // depthGeometry.translate(-this.params.meanLon, 0, this.params.meanLat);
+    // depthGeometry.scale(10000, 1, 10000);
+    // boatGeometry.translate(-this.params.meanLon, 0, this.params.meanLat);
+    // boatGeometry.scale(10000, 1, 10000);
 
     // マテリアル
     const depthMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
@@ -303,8 +351,93 @@ export class Main {
   }
 
 
+  createArrow() {
+    // 円錐と円柱を作成して結合する
+
+    // 円錐
+    const coneGeometry = new THREE.ConeGeometry(1, 2, 10);
+
+    // 円柱
+    const cylinderGeometry = new THREE.CylinderGeometry(0.4, 0.6, 3, 10);
+
+    // 円柱の位置を上に調整
+    coneGeometry.translate(0, 2.5, 0);
+
+    // 結合する
+    // .mergeGeometries ( geometries : Array, useGroups : Boolean ) : BufferGeometry
+    const arrowGeometry = mergeGeometries([coneGeometry, cylinderGeometry]);
+
+    // サイズを調整
+    arrowGeometry.scale(0.5, 0.5, 0.5);
+
+    // マテリアル
+    const material = new THREE.MeshNormalMaterial();
+
+    // メッシュ化
+    this.arrow = new THREE.Mesh(arrowGeometry, material);
+
+    this.arrow.position.set(0, 0, 0);
+
+    // シーンに追加
+    this.scene.add(this.arrow);
+  }
+
+  createVerticalLine() {
+    const geometry = new THREE.BufferGeometry();
+
+    const vertices = new Float32Array([
+      0,  // start x
+      0,  // start y
+      0,  // start z
+
+      0,    // end x
+      -10,  // end y ★ geometry.attributes.position.array[4] でここを更新
+      0     // end z
+    ]);
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+    const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+
+    this.verticalLine = new THREE.Line(geometry, material);
+
+    this.scene.add(this.verticalLine);
+  }
+
+
+
+  UP = new THREE.Vector3(0, 1, 0);
+  AXIS = new THREE.Vector3();
 
   updatePosition() {
+
+    // 配列のインデックスを計算
+    const index = Math.floor(this.params.fraction * (this.params.numPoints - 1));
+
+    // 位置を取得
+    const position = this.params.boatPoints[index];
+
+    // 位置を更新
+    this.arrow.position.copy(position);
+
+    // 向きを更新
+    const tangent = this.params.boatTangents[index];
+    this.AXIS.crossVectors(this.UP, tangent).normalize();
+    const radians = Math.acos(this.UP.dot(tangent));
+    this.arrow.quaternion.setFromAxisAngle(this.AXIS, radians);
+
+    // 水深を取得
+    const depthPosition = this.params.depthPoints[index];
+
+    // 垂直線の位置を更新
+    this.verticalLine.position.copy(position);
+    this.verticalLine.geometry.attributes.position.array[4] = depthPosition.y;
+    this.verticalLine.geometry.attributes.position.needsUpdate = true;
+
+    this.params.fraction += 0.001;
+    if (this.params.fraction > 1) {
+      this.params.fraction = 0.0;
+    }
 
   }
 
