b8dfaaeaaff8803212039bbe78c82ba69bc9f77b
takamitsu-iida <takamitsu.iida@gmail.com>
2024-10-29 20:16:45 +0900

    add delaunary triangulation

--- static/site/js/load-csv-qtree.js
+++ static/site/js/load-csv-qtree.js
@@ -7,6 +7,40 @@ import { GUI } from "three/libs/lil-gui.module.min.js";
 // stats.js
 import Stats from "three/libs/stats.module.js";
 
+// デローネ三角形
+import Delaunator from "delaunatorjs";
+
+/*
+import Delaunator from "delaunatorjs";
+を実現するには、ちょっと苦労がある。
+
+https://github.com/mapbox/delaunator
+ここからReleasesの最新版（2024年9月時点でv5.0.1）をダウンロードする。
+この中にindex.jsがあるので、これを使う。
+
+delaunatorは内部でrobust-predicatesのorient2dを使っているため、
+orient2dが見つからないというエラーが発生する。
+
+https://github.com/mourner/robust-predicates
+ここからReleasesの最新版（2024年9月時点でv3.0.2）をダウンロードする。
+この中のsrcフォルダのjavascriptファイルをコピーして使う。
+
+HTMLではこのようなimportmapを使う。
+
+<!-- three.js -->
+<script type="importmap">
+  {
+    "imports": {
+      "three": "./static/build/three.module.js",
+      "three/libs/": "./static/libs/",
+      "three/controls/": "./static/controls/",
+      "robust-predicates": "./static/libs/robust-predicates-3.0.2/orient2d.js",
+      "delaunatorjs": "./static/libs/delaunator-5.0.1/index.js"
+    }
+  }
+</script>
+
+*/
 
 export class Main {
 
@@ -34,11 +68,22 @@ export class Main {
     path: "./static/data/depth_map_data.csv",
     csvData: null,
 
+    wireframe: false,
     autoRotate: true,
     autoRotateSpeed: 1.0,
+
+    minLat: 0,
+    maxLat: 0,
+    minLon: 0,
+    maxLon: 0,
+
+    // 四分木に分割した領域の配列
+    areas: null,
   }
 
-  pointCloud;
+  // デローネ三角形で作成する地形図のメッシュ（guiで表示を操作するためにインスタンス変数にする）
+  terrainMesh;
+
 
   constructor(params = {}) {
     this.params = Object.assign(this.params, params);
@@ -55,8 +100,11 @@ export class Main {
       return;
     }
 
-    console.log(this.params.csvData);
+    // console.log(this.params.csvData);
     // {lat: xxx, lon: xxx, depth: xxx}
+
+    // quad treeに分割する
+    this.initQuadTree();
 
     // scene, camera, renderer, controllerを初期化
     this.initThreejs();
@@ -67,8 +115,8 @@ export class Main {
     // stats.jsを初期化
     this.initStatsjs();
 
-    // CSVデータを元に点群を初期化
-    this.initPointCloud();
+    // 四分木に分割したデータareasを元にデローネ三角形を形成する
+    this.initDelaunay();
 
     // フレーム毎の処理(requestAnimationFrameで再帰的に呼び出される)
     this.render();
@@ -119,6 +167,11 @@ export class Main {
     const headers = lines[0].split(',');
     const data = [];
 
+    let minLat = 9999;
+    let maxLat = -9999;
+    let minLon = 9999;
+    let maxLon = -9999;
+
     for (let i = 1; i < lines.length; i++) {
       const row = lines[i].split(',');
       if (row.length === headers.length) {
@@ -128,8 +181,29 @@ export class Main {
         }
         this.normalizeData(rowData);
         data.push(rowData);
+
+        // 緯度経度の最大値、最小値を取得
+        if (rowData.lat < minLat) {
+          minLat = rowData.lat;
+        }
+        if (rowData.lat > maxLat) {
+          maxLat = rowData.lat;
+        }
+        if (rowData.lon < minLon) {
+          minLon = rowData.lon;
+        }
+        if (rowData.lon > maxLon) {
+          maxLon = rowData.lon;
+        }
       }
     }
+
+    console.log(`minLat: ${minLat}\nmaxLat: ${maxLat}\nminLon: ${minLon}\nmaxLon: ${maxLon}`);
+
+    this.params.minLat = minLat;
+    this.params.maxLat = maxLat;
+    this.params.minLon = minLon;
+    this.params.maxLon = maxLon;
 
     return data;
   }
@@ -202,6 +276,14 @@ export class Main {
     const axesHelper = new THREE.AxesHelper(10000);
     this.scene.add(axesHelper);
 
+    // 環境光
+    this.scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+
+    // ディレクショナルライト
+    const light = new THREE.DirectionalLight(0xffffff, 0.8);
+    light.position.set(-50, 50, 50);
+    this.scene.add(light);
+
   }
 
 
@@ -214,20 +296,28 @@ export class Main {
 
     gui
       .add(this.params, "autoRotate")
-      .name("rotation")
+      .name(navigator.language.startsWith("ja") ? "自動回転" : "rotation")
       .onChange((value) => {
         this.controller.autoRotate = value;
       });
 
     gui
       .add(this.params, "autoRotateSpeed")
-      .name("autoRotateSpeed")
+      .name(navigator.language.startsWith("ja") ? "回転スピード" : "autoRotateSpeed")
       .min(1.0)
       .max(10.0)
       .step(0.1)
       .onChange((value) => {
         this.controller.autoRotateSpeed = value;
       });
+
+    gui
+      .add(this.params, "wireframe")
+      .name(navigator.language.startsWith("ja") ? "ワイヤーフレーム表示" : "wireframe")
+      .onChange(() => {
+        this.terrainMesh.material.wireframe = this.params.wireframe;
+      });
+
 
   }
 
@@ -283,52 +373,24 @@ export class Main {
   };
 
 
-  initPointCloud = () => {
-    const csvData = this.params.csvData;
-
-    const vertices = new Float32Array((csvData.length) * 3);
-
-    csvData.forEach((obj, index) => {
-      vertices[index * 3 + 0] = obj.lon;    // 経度
-      vertices[index * 3 + 1] = obj.depth;  // 水深
-      vertices[index * 3 + 2] = obj.lat;    // 緯度
-    });
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-
-    // 点群のマテリアルを作成
-    const material = new THREE.PointsMaterial({
-      color: 0x99ccff,
-      size: 0.1,
-    });
-
-    // 点群を作成
-    this.pointCloud = new THREE.Points(geometry, material);
-
-    this.scene.add(this.pointCloud);
-
-  }
-
-
   quadtree = (area, maxpoints, maxdivision) => {
     if (area.points.length <= maxpoints || area.depth >= maxdivision) {
       return [area];
     }
 
-    const midX = (area.x1 + area.x2) / 2;
-    const midY = (area.y1 + area.y2) / 2;
+    const midX = (area.minLon + area.maxLon) / 2;
+    const midZ = (area.minLat + area.maxLat) / 2;
 
     const areas = [
-      new Area(area.x1, area.y1, midX, midY, area.depth + 1),
-      new Area(midX, area.y1, area.x2, midY, area.depth + 1),
-      new Area(area.x1, midY, midX, area.y2, area.depth + 1),
-      new Area(midX, midY, area.x2, area.y2, area.depth + 1)
+      new Area(area.minLon, area.minLat, midX, midZ, area.depth + 1),
+      new Area(midX, area.minLat, area.maxLon, midZ, area.depth + 1),
+      new Area(area.minLon, midZ, midX, area.maxLat, area.depth + 1),
+      new Area(midX, midZ, area.maxLon, area.maxLat, area.depth + 1)
     ];
 
     for (const point of area.points) {
       for (const subArea of areas) {
-        if (point.lat >= subArea.x1 && point.lat < subArea.x2 && point.lon >= subArea.y1 && point.lon < subArea.y2) {
+        if (point.lat >= subArea.minLat && point.lat < subArea.maxLat && point.lon >= subArea.minLon && point.lon < subArea.maxLon) {
           subArea.append(point);
           break;
         }
@@ -337,46 +399,118 @@ export class Main {
 
     let result = [];
     for (const subArea of areas) {
-      result = result.concat(quadtree(subArea, maxpoints, maxdivision));
+      result = result.concat(this.quadtree(subArea, maxpoints, maxdivision));
     }
 
     return result;
   }
 
-  dumpAreas = (areas) => {
-    for (const area of areas) {
-      console.log(`${area.x1} ${area.y1} ${area.x2} ${area.y2}`);
-      for (const point of area.points) {
-        console.log(point);
-      }
-    }
-  }
-
 
   initQuadTree = () => {
-    const dataPath = path.join(__dirname, 'data.csv');
-    const data = readCSV(dataPath);
+    if (this.params.areas !== null) {
+      return;
+    }
 
-    const x_min = 35.1572;
-    const y_min = 139.599;
-    const x_max = 35.1737;
-    const y_max = 139.622;
+    // X軸 = Longitude(経度)
+    const minLon = this.params.minLon;  // -93.15585150687866
+    const maxLon = this.params.maxLon;  // 137.8217825273964
+
+    // Z軸 = Latitude(緯度)
+    const minLat = this.params.minLat;  // -96.32579062369473
+    const maxLat = this.params.maxLat;  // 68.51506401226004
+
+    // 5で割るのは適当に選んだ
+    const maxdivision = Math.ceil(Math.max((maxLon - minLon) / 5, (maxLat - minLat) / 5));
+    console.log(`maxdivision: ${maxdivision}`);
+
     const maxpoints = 5;
-    const maxdivision = 20;
 
     // 対象とする領域を生成
-    const initial = new Area(x_min, y_min, x_max, y_max, 0);
+    const initial = new Area(minLon, minLat, maxLon, maxLat, 0);
+
+    const data = this.params.csvData;
+    // dataは配列で、各要素は以下のようなオブジェクト
+    // [ {lat: 67.88624331335313, lon: -81.94761236723025, depth: -21.1785}, {...}, ... ]
+    // 初期領域にデータを追加する
     for (const d of data) {
       initial.append(d);
     }
 
-    // 分割
-    const areas = quadtree(initial, maxpoints, maxdivision);
+    // 四分木に分割する
+    const areas = this.quadtree(initial, maxpoints, maxdivision);
+    console.log(`initial area is divided into ${areas.length} areas`);
 
-    // 結果
-    dumpArea(areas);
+    // 保存しておく
+    this.params.areas = areas;
+  }
 
-    return 0;
+
+  initDelaunay = () => {
+
+    // 四分木で分割した領域を一つの点とするポイントクラウドを作成する
+    const areas = this.params.areas;
+
+    const point3d = [];
+    areas.forEach((area) => {
+      if (area.points.length === 0) {
+        return;
+      }
+      const lon = (area.minLon + area.maxLon) / 2;
+      const lat = (area.minLat + area.maxLat) / 2;
+      const depth = area.points.reduce((acc, cur) => acc + cur.depth, 0) / area.points.length;
+      point3d.push(new THREE.Vector3(lon, depth, lat));
+    });
+
+    console.log(`${point3d.length} points are created`);
+
+    // ポイントクラウドのジオメトリを作成
+    const geometry = new THREE.BufferGeometry().setFromPoints(point3d);
+
+    // マテリアルを作成
+    const pointMaterial = new THREE.PointsMaterial({
+      color: 0x99ccff,
+      size: 0.2,
+    });
+
+    // 点群を作成
+    const pointCloud = new THREE.Points(geometry, pointMaterial);
+
+    // シーンに追加
+    this.scene.add(pointCloud);
+
+    // デローネ三角形を形成する
+    const delaunay = Delaunator.from(
+      point3d.map(v => {
+        return [v.x, v.z];
+      })
+    );
+
+    // デローネ三角形のインデックスをmeshIndexに代入してThree.jsのインデックスに変換
+    const meshIndex = [];
+    for (let i = 0; i < delaunay.triangles.length; i++) {
+      meshIndex.push(delaunay.triangles[i]);
+    }
+
+    // 点群のジオメトリにインデックスを追加してポリゴン化
+    geometry.setIndex(meshIndex);
+
+    // 法線ベクトルを計算
+    geometry.computeVertexNormals();
+
+    // マテリアルを生成
+    const material = new THREE.MeshLambertMaterial({
+      color: 0x00ff00,
+      wireframe: this.params.wireframe,
+    });
+
+    // メッシュを生成
+    const terrainMesh = new THREE.Mesh(geometry, material);
+
+    // シーンに追加
+    this.scene.add(terrainMesh);
+
+    // インスタンス変数に保存
+    this.terrainMesh = terrainMesh;
   }
 
 
@@ -385,13 +519,27 @@ export class Main {
 
 
 class Area {
-  constructor(x1, y1, x2, y2, depth) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  // Lon = X軸
+  minLon;
+  maxLon;
+
+  // Lat = Z軸
+  minLat;
+  maxLat;
+
+  // 四分木の深さ
+  depth;
+
+  // この領域に含まれる点の配列
+  // [{lat: xxx, lon: xxx, depth: xxx}, ...]
+  points = [];
+
+  constructor(minLon, minLat, maxLon, maxLat, depth) {
+    this.minLon = minLon;
+    this.minLat = minLat;
+    this.maxLon = maxLon;
+    this.maxLat = maxLat;
     this.depth = depth;
-    this.points = [];
   }
 
   append(point) {
