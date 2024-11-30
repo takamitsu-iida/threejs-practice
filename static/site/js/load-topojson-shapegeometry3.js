4772ab05caea92b6a20edbd0975232c2825d170d
takamitsu-iida <takamitsu.iida@gmail.com>
2024-11-30 20:51:44 +0900

    temp save

--- static/site/js/load-topojson-shapegeometry3.js
+++ static/site/js/load-topojson-shapegeometry3.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/controls/OrbitControls.js";
+// import { OrbitControls } from "three/controls/OrbitControls.js";
+import { MapControls } from "three/controls/MapControls.js";
+import { TrackballControls } from "three/controls/TrackballControls.js";
 
 // stats.js
 import Stats from "three/libs/stats.module.js";
@@ -19,22 +21,6 @@ import Stats from "three/libs/stats.module.js";
 //       "geometries": [
 
 
-const geo_to_vec3 = (longitude, latitude, radius) => {
-  // x = rcos(latitude)cos(longitude)
-  // y = rsin(latitude)
-  // z = −rcos(latitude)sin(longitude)
-
-  // 経度と緯度をラジアンに変換
-  latitude = latitude * Math.PI / 180.0;
-  longitude = longitude * Math.PI / 180.0;
-
-  return new THREE.Vector3(
-    radius * Math.cos(latitude) * Math.cos(longitude),
-    radius * Math.sin(latitude),
-    -radius * Math.cos(latitude) * Math.sin(longitude));
-}
-
-
 export class Main {
 
   container;
@@ -47,19 +33,25 @@ export class Main {
   scene;
   camera;
   renderer;
-  controller;
-
+  mapControls;
+  zoomControls;
   statsjs;
+
+  // マウス座標
+  mousePosition = new THREE.Vector2();
+
+  // マウス座標（1フレーム前）
+  previousMousePosition = new THREE.Vector2();
 
   renderParams = {
     clock: new THREE.Clock(),
     delta: 0,
     interval: 1 / 30,  // = 30fps
+
+    distance: null,
   }
 
   params = {
-    radius: 128,
-
     jsonData: null,
 
     // ファイルのパス
@@ -70,6 +62,19 @@ export class Main {
 
     // translate
     translate: [0.0, 0.0],
+
+    // グリッドを走査するときの、グリッドのスケール
+    // 500m x gridScale の範囲でデータを取得する
+    gridScale: 2.0,
+
+    // (lon, lat)をThree.jsのXZ座標のどの範囲に描画するか
+    xzGridSize: 800,  // 800を指定する場合は -400～400 の範囲に描画する
+
+    // xzGridSizeにあわせるために、どのくらい緯度経度の値を拡大するか（自動で計算する）
+    xzScale: 1,
+
+
+
   }
 
 
@@ -77,6 +82,7 @@ export class Main {
     this.params = Object.assign(this.params, params);
     this.init();
   }
+
 
   async init() {
     // データを読み込む
@@ -174,28 +180,64 @@ export class Main {
       60,
       this.sizes.width / this.sizes.height,
       1,
-      1000
+      10000
     );
-
-    // 東京の上空にカメラを設定
-    const tokyo = [139.692, 35.689];
-    const tokyoPosition = geo_to_vec3(tokyo[0], tokyo[1], this.params.radius * 2.5);
-    this.camera.position.set(tokyoPosition.x, tokyoPosition.y, tokyoPosition.z);
+    this.camera.position.set(0, 1000, 0);
 
     // レンダラ
-    this.renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      alpha: true,
-    });
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize(this.sizes.width, this.sizes.height);
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    // 地図は大きいのでクリッピングを有効にして表示領域を制限する
+    this.renderer.localClippingEnabled = true;
+
+    // コンテナにレンダラを追加
     this.container.appendChild(this.renderer.domElement);
 
     // コントローラ
-    this.controller = new OrbitControls(this.camera, this.renderer.domElement);
-    // this.controller.enableZoom = false; // ズームを無効化
-    // this.controller.enableRotate = false; // 回転を無効化
-    // this.controller.enablePan = false; // パンを無効化
+    this.mapControls = new MapControls(this.camera, this.renderer.domElement);
+    this.mapControls.enableDamping = false;
+    this.mapControls.enableZoom = false;
+    this.mapControls.maxDistance = 1000;
+
+    this.zoomControls = new TrackballControls(this.camera, this.renderer.domElement);
+    this.zoomControls.noPan = true;
+    this.zoomControls.noRotate = true;
+    this.zoomControls.staticMoving = true;
+
+    this.zoomControls.addEventListener('change', (event) => {
+      const distance = this.getDistance();
+      if (this.renderParams.distance !== distance) {
+        this.renderParams.distance = distance;
+        // console.log(`distance: ${distance}`);
+
+        // distance が 0 のとき gridScale は 1、distance が 1000 のとき gridScale は 2 になるように線形補間
+        // const gridScale = 1 + (distance / 1000);
+
+        // distance に基づいて gridScale を階段状に設定
+        let gridScale;
+        if (distance < 500) {
+          gridScale = 1.0;
+        } else if (distance < 600) {
+          gridScale = 1.2;
+        } else if (distance < 700) {
+          gridScale = 1.4;
+        } else if (distance < 800) {
+          gridScale = 1.6;
+        } else if (distance < 900) {
+          gridScale = 1.8;
+        } else {
+          gridScale = 2.0;
+        }
+
+        if (this.params.gridScale !== gridScale) {
+          this.params.gridScale = gridScale;
+
+          console.log(`gridScale: ${gridScale}`);
+        }
+      }
+    });
 
     // 軸を表示
     //
@@ -205,8 +247,22 @@ export class Main {
     //   /
     //  Z(blue)
     //
-    const axesHelper = new THREE.AxesHelper(this.params.radius);
+    const axesHelper = new THREE.AxesHelper(this.params.xzGridSize);
     this.scene.add(axesHelper);
+
+    // 環境光
+    this.scene.add(new THREE.AmbientLight(0xffffff, 0.6));
+
+    // ディレクショナルライト
+    const light = new THREE.DirectionalLight(0xffffff, 0.8);
+    light.position.set(-50, 0, 0);
+    this.scene.add(light);
+
+    // 正規化したマウス座標を保存
+    this.renderer.domElement.addEventListener("mousemove", (event) => {
+      this.mousePosition.x = (event.clientX / this.sizes.width) * 2 - 1;
+      this.mousePosition.y = -(event.clientY / this.sizes.height) * 2 + 1;
+    }, false);
 
   }
 
@@ -226,6 +282,7 @@ export class Main {
   }
 
 
+
   render = () => {
     // 再帰処理
     requestAnimationFrame(this.render);
@@ -239,8 +296,10 @@ export class Main {
       // stats.jsを更新
       this.statsjs.update();
 
-      // カメラコントローラーの更新
-      this.controller.update();
+      // カメラコントローラーを更新
+      this.mapControls.update();
+      this.zoomControls.target.set(this.mapControls.target.x, this.mapControls.target.y, this.mapControls.target.z);
+      this.zoomControls.update();
 
       // 再描画
       this.renderer.render(this.scene, this.camera);
@@ -262,7 +321,6 @@ export class Main {
 
     // topojsonのFeatureCollectionからFeatureを一つずつ取り出す
     // featureには一つの県の情報が格納されている
-
     features.forEach(feature => {
       prefectures.push(new Prefecture({
         feature: feature,
@@ -276,45 +334,37 @@ export class Main {
 
   drawPrefectures = (prefectures) => {
 
-    // 全体のバウンディングボックス
-    const overallBoundingBox = new THREE.Box3();
-
     prefectures.forEach(prefecture => {
-      const mesh = prefecture.mesh;
-
-      // このメッシュのバウンディングボックスを計算
-      const boundingBox = new THREE.Box3().setFromObject(mesh);
-
-      // 全体のバウンディングボックスに結合
-      overallBoundingBox.union(boundingBox);
+      const mesh = prefecture.shapeMesh;
 
       // メッシュをシーンに追加
       this.scene.add(mesh);
+
+      // 線分をシーンに追加
+      prefecture.lineSegments.forEach(line => {
+        this.scene.add(line);
+      });
+
     });
 
-    // console.log(overallBoundingBox);
-
-    /*
-    // カメラの位置を調整
-    const center = new THREE.Vector3();
-    overallBoundingBox.getCenter(center);
-    this.camera.position.set(center.x, center.y, this.camera.position.z);
-    this.camera.lookAt(center.x, center.y, this.controller.target.z);
-
-    // ズームを調整
-    this.camera.zoom = Math.min(
-      this.sizes.width / (overallBoundingBox.max.x - overallBoundingBox.min.x),
-      this.sizes.height / (overallBoundingBox.max.y - overallBoundingBox.min.y)
-    ) * 1.2;
-    this.camera.updateProjectionMatrix();
-    this.camera.updateMatrix();
-
-    // コントローラのターゲットを調整
-    this.controller.target.set(center.x, center.y, this.controller.target.z);
-    this.controller.update();
-    */
-
   }
+
+
+  getDistance = () => {
+    // TrackballControlsにはgetDistance()がない
+    return this.camera.position.distanceTo(this.mapControls.target);
+  }
+
+
+  getZoom = () => {
+    // OrbitControlsにはgetDistance()がある
+    if (this.renderParams.distance === null) {
+      this.renderParams.distance = this.zoomControls.getDistance();
+    }
+    return this.renderParams.distance / this.zoomControls.getDistance();
+  }
+
+
 
 }
 
@@ -324,14 +374,22 @@ class Prefecture {
   nam = "";
   name_ja = "";
 
-  mesh;
+  // THREE.Shapeの配列
+  shapes = [];
+
+  // THREE.Mesh
+  shapeMesh;
+
+  // Vector3の配列の配列
+  linePoints = [];
+
+  // THREE.LineSegmentsの配列
+  lineSegments = [];
 
   params = {
-    // 画面上の半径
-    radius: 128,
-
-    // TopoJSONのfeature
+    depth: 0.1,
     feature: null,
+    translate: [0.0, 0.0],
   }
 
 
@@ -343,92 +401,160 @@ class Prefecture {
     this.nam = feature.properties.nam;
     this.nam_ja = feature.properties.nam_ja;
 
-    const geometry = this.createLineSegmentsGeometry(feature);
+    this.parseFeature(feature);
 
-    const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-
-    const mesh = new THREE.LineSegments(geometry, material);
-
-    this.mesh = mesh;
+    this.createMesh();
   }
 
 
-  createLineSegmentsGeometry = (feature) => {
-
-    // LineSegmentsの頂点座標を格納する配列
-    const positions = [];
-
-    // 画面上の球体の半径
-    const radius = this.params.radius;
+  parseFeature = (feature) => {
 
     // GeometryがLineStringの場合
     if (feature.geometry.type === 'LineString') {
+      const shape = new THREE.Shape();
+      const points = [];
 
       const coordinates = feature.geometry.coordinates;
 
-      // coordinatesは[[lon, lat], [lon, lat], ...]の配列
-      // [lon, lat]の部分をTHREE.Vector3に変換する
-      const vertecies = coordinates.map(coords => {
-        const lon = coords[0];
-        const lat = coords[1];
-        return geo_to_vec3(lon, lat, radius);
-      });
+      // パスを開始
+      shape.moveTo(
+        coordinates[0][0],
+        coordinates[0][1]
+      );
 
-      // THREE.LineSegmentsは2点を結ぶ線分を描画する
-      // indexを省略する場合、(p0, p1), (p1, p2), (p2, p3), ... というように頂点を登録する
-      // 頂点をp0, p1, p2... のように登録するならindexを[0, 1, 1, 2, 2, 3...]と指定する必要がある
-      // 簡単なのは前者なので、ここでは前者を採用する
-      for (let i = 0; i < vertecies.length - 1; i++) {
-        positions.push(vertecies[i].x, vertecies[i].y, vertecies[i].z);
-        positions.push(vertecies[i + 1].x, vertecies[i + 1].y, vertecies[i + 1].z);
+      // 線分の始点
+      let p0 = new THREE.Vector3(coordinates[0][0], coordinates[0][1], 0);
+
+      for (let i = 1; i < coordinates.length; i++) {
+        // 線分を追加
+        shape.lineTo(
+          coordinates[i][0],
+          coordinates[i][1]
+        );
+
+        // 線分の終点
+        let p1 = new THREE.Vector3(coordinates[i][0], coordinates[i][1], 0);
+
+        points.push(p0, p1);
+        p0 = p1;
       }
+
+      // Shapeを追加
+      this.shapes.push(shape);
+
+      // pointsを追加
+      this.linePoints.push(points);
     }
 
     // GeometryがPolygonの場合
     else if (feature.geometry.type === 'Polygon') {
+      const shape = new THREE.Shape();
+      const points = [];
 
       const coordinates = feature.geometry.coordinates[0];
 
-      const vertecies = coordinates.map(coords => {
-        const lon = coords[0];
-        const lat = coords[1];
-        return geo_to_vec3(lon, lat, radius);
-      });
+      shape.moveTo(
+        coordinates[0][0],
+        coordinates[0][1]
+      );
 
-      for (let i = 0; i < vertecies.length - 1; i++) {
-        positions.push(vertecies[i].x, vertecies[i].y, vertecies[i].z);
-        positions.push(vertecies[i + 1].x, vertecies[i + 1].y, vertecies[i + 1].z);
+      let p0 = new THREE.Vector3(coordinates[0][0], coordinates[0][1], 0);
+
+      for (let i = 1; i < coordinates.length; i++) {
+        shape.lineTo(
+          coordinates[i][0],
+          coordinates[i][1]
+        );
+
+        let p1 = new THREE.Vector3(coordinates[i][0], coordinates[i][1], 0);
+
+        points.push(p0, p1);
+        p0 = p1;
       }
+
+      this.shapes.push(shape);
+
+      this.linePoints.push(points);
     }
 
     // GeometryがMultiPolygonの場合
     else if (feature.geometry.type === 'MultiPolygon') {
       feature.geometry.coordinates.forEach(polygon => {
+        const shape = new THREE.Shape();
+        const points = [];
 
         const coordinates = polygon[0];
 
-        const vertecies = coordinates.map(coords => {
-          const lon = coords[0];
-          const lat = coords[1];
-          return geo_to_vec3(lon, lat, radius);
-        });
+        shape.moveTo(
+          coordinates[0][0],
+          coordinates[0][1]
+        );
 
+        let p0 = new THREE.Vector3(coordinates[0][0], coordinates[0][1], 0);
 
-        for (let i = 0; i < vertecies.length - 1; i++) {
-          positions.push(vertecies[i].x, vertecies[i].y, vertecies[i].z);
-          positions.push(vertecies[i + 1].x, vertecies[i + 1].y, vertecies[i + 1].z);
+        for (let i = 1; i < coordinates.length; i++) {
+          shape.lineTo(
+            coordinates[i][0],
+            coordinates[i][1]
+          );
+
+          let p1 = new THREE.Vector3(coordinates[i][0], coordinates[i][1], 0);
+
+          points.push(p0, p1);
+          p0 = p1;
         }
 
+        this.shapes.push(shape);
+
+        this.linePoints.push(points);
       });
     }
 
-    // LineSegmentsのgeometryを作成
-    const geometry = new THREE.BufferGeometry();
+  }
 
-    // Float32Arrayに変換してgeometryのposition属性に頂点座標をセットする
-    geometry.setAttribute("position", new THREE.BufferAttribute(new Float32Array(positions), 3));
 
-    return geometry;
+  createMesh = () => {
+    // ShapeGeometryを作成
+    const geometry = new THREE.ShapeGeometry(this.shapes);
+
+    // マテリアル、ここでは適当にMeshBasicMaterialを使う
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      side: THREE.DoubleSide
+    });
+
+    // 移動して原点に寄せる
+    // 中心を原点に寄せるためには、もう少し左、もう少し下に移動する必要がある
+    geometry.translate(-this.params.translate[0], -this.params.translate[1], 0);
+
+    // メッシュ化
+    const mesh = new THREE.Mesh(geometry, material);
+
+    // メッシュをインスタンス変数に保存
+    this.shapeMesh = mesh;
+
+    // メッシュを選択可能にする
+    mesh.selectable = true;
+
+    // ユーザーデータを追加
+    mesh.userData = {
+      nam: this.nam,
+      nam_ja: this.nam_ja,
+    };
+
+    // 線分を作成
+    this.linePoints.forEach(points => {
+      // LineSegmentsのgeometryを作成
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+      geometry.translate(-this.params.translate[0], -this.params.translate[1], 0);
+      const material = new THREE.LineBasicMaterial({ color: 0x000000 });
+      const lineSegments = new THREE.LineSegments(geometry, material);
+
+      // ラインセグメントは選択不可
+      lineSegments.selectable = false;
+
+      this.lineSegments.push(lineSegments);
+    });
   }
 
 }
